e8ecd61503bf47225c85d4c37b96c084c4754be6
unknown <iskandar.django.work@gmail.com>
2026-07-25 08:15:10 +0500

    feat(gui): add result table component

---
+++ src/components/result-table.tsx
@@ -0,0 +1,41 @@
+type ResultTableProps = {
+  data: Record<string, any>[];
+};
+
+export const ResultTable = ({ data }: ResultTableProps) => {
+  const headers = Object.keys(data[0]);
+  return (
+    <div className="result-div">
+      <table className="result-table">
+        <thead className="table-header">
+          <tr className="header-tr">
+            {headers.map((header) => {
+              return (
+                <th key={header} className="header-th">
+                  {header}
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody className="table-body">
+          {data.map((row, index) => {
+            return (
+              <tr key={row.id || index} className="body-tr">
+                {headers.map((header) => {
+                  return (
+                    <td key={header} className="body-td">
+                      {typeof row[header] == "object"
+                        ? JSON.stringify(row[header])
+                        : String(row[header])}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
