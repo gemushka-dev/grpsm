39cb3eb92f4675cf1f1050df2acbcdaec6abffc6
unknown <iskandar.django.work@gmail.com>
2026-07-25 10:46:36 +0500

    feat: wire up dashboard components in main app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import { Header } from "./components/header";
+import { Sidebar } from "./components/sidebar";
+import { EditorComponent } from "./components/sql-editor";
+import { ResultTable } from "./components/result-table";
 import "./styles/base.css";
+import "./styles/main.css";
 
 export const App = () => {
-  return <h1>GRPSM v.0.1.0</h1>;
+  const [activeConnection, setActiveConnection] = useState(null);
+  return (
+    <div className="container">
+      <Header></Header>
+
+      <div className="main-part">
+        <Sidebar
+          pgConnections={[]}
+          onNodeSelect={(conn) => setActiveConnection(conn)}
+        ></Sidebar>
+
+        <div className="sql-part">
+          <EditorComponent></EditorComponent>
+          <ResultTable data={[]}></ResultTable>
+        </div>
+      </div>
+    </div>
+  );
 };
