5751478b26d17a4fc4c6d02ed86651b4248c5dd8
unknown <iskandar.django.work@gmail.com>
2026-07-25 08:34:25 +0500

    feat(gui): add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+export const Header = () => {
+  return (
+    <header className="header">
+      <h3 className="header-logo">grpsm</h3>
+      <ul className="header-list">
+        <li className="list-item">
+          <button className="item-connection">➕</button>
+        </li>
+        <li className="list-item">
+          <button className="item-start">⏩</button>
+        </li>
+      </ul>
+    </header>
+  );
+};
