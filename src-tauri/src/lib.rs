15c8a311dbe893f51ea054c2bf7f29ed41196918
unknown <iskandar.django.work@gmail.com>
2026-07-26 13:56:14 +0500

    feat(sql): add tauri command for execution sql script

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
-use sqlx::{PgPool};
+use sqlx::{PgPool, Row, Column};
 use uuid::Uuid;
 use tokio::sync::RwLock;
+use serde_json::{json, Value};
 
 pub struct AppState {
     pub connections: RwLock<HashMap<String, PgPool>>,
@@ -19,6 +20,31 @@ async fn connect_to_db(connection_string: String, state: tauri::State<'_, AppSta
     Ok(connection_id)
 }
 
+#[tauri::command]
+async fn execute_query(uuid: String, sql:String, state: tauri::State<'_, AppState>) -> Result<Value, String>{
+    let read_connections = state.connections.read().await;
+    let connection = read_connections.get(&uuid).ok_or_else(|| "No connection".to_string())?;
+
+    match sqlx::query(&sql).fetch_all(connection).await{
+        Ok(rows) => {
+            let res: Vec<Value> = rows.iter().map(|row| {
+                let mut map = serde_json::Map::new();
+                for col in row.columns(){
+                    let val: Option<String> = row.try_get(col.name()).ok();
+                    map.insert(col.name().to_string(), json!(val));
+                }
+                Value::Object(map)
+            }).collect();
+            Ok(json!({"type":"select", "data":res}))
+        },
+        Err(_) => {
+            let execution = sqlx::query(&sql).execute(connection).await.map_err(|e| e.to_string())?;
+            Ok(json!({ "type": "mutation", "rows_affected": execution.rows_affected() }))
+        }
+    }
+}
+
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -26,7 +52,7 @@ pub fn run() {
         .manage(AppState{
             connections: RwLock::new(HashMap::new(),)
         })
-        .invoke_handler(tauri::generate_handler![connect_to_db])
+        .invoke_handler(tauri::generate_handler![connect_to_db, execute_query])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
