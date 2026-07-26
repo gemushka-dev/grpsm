d78c6ca4c4e19ca147d61921995f52f2a07704f9
unknown <iskandar.django.work@gmail.com>
2026-07-26 13:19:50 +0500

    feat(sql): add tauri command for connection to database

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,32 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::collections::HashMap;
+use sqlx::{PgPool};
+use uuid::Uuid;
+use tokio::sync::RwLock;
+
+pub struct AppState {
+    pub connections: RwLock<HashMap<String, PgPool>>,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn connect_to_db(connection_string: String, state: tauri::State<'_, AppState>) -> Result<String, String>{
+    let pool = PgPool::connect(&connection_string)
+        .await
+        .map_err(|e| e.to_string())?;
+    let connection_id = Uuid::new_v4().to_string();
+    let mut connections = state.connections.write().await;
+    connections.insert(connection_id.clone(), pool);
+
+    Ok(connection_id)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(AppState{
+            connections: RwLock::new(HashMap::new(),)
+        })
+        .invoke_handler(tauri::generate_handler![connect_to_db])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
