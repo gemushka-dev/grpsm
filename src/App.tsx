d8f621603d2fdaa620114359d6d6f24bfef8f896
unknown <iskandar.django.work@gmail.com>
2026-07-27 13:00:44 +0500

    feat(connect-db): integrate Connection Model component into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Header } from "./components/header";
 import { Sidebar } from "./components/sidebar";
 import { EditorComponent } from "./components/sql-editor";
 import { ResultTable } from "./components/result-table";
+import { type Connection } from "./type/connection.type";
+import { ConnectionModel } from "./components/connection";
+import Draggable from "react-draggable";
 import "./styles/base.css";
 import "./styles/main.css";
 
 export const App = () => {
   const [activeConnection, setActiveConnection] = useState(null);
+  const [connections, setConnections] = useState<Connection[]>([]);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const nodeRef = useRef<HTMLDivElement>(null);
+
   return (
     <div className="container">
-      <Header></Header>
+      <Header isVisible={isVisible} setIsVisible={setIsVisible}></Header>
+      {isVisible ? (
+        <Draggable cancel="" nodeRef={nodeRef}>
+          <div ref={nodeRef} className="drag-div" style={{}}>
+            <ConnectionModel setConnections={setConnections} />
+          </div>
+        </Draggable>
+      ) : null}
 
       <div className="main-part">
         <Sidebar
-          pgConnections={[]}
+          pgConnections={connections}
           onNodeSelect={(conn) => setActiveConnection(conn)}
         ></Sidebar>
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,22 @@
 import "../styles/header.css";
-export const Header = () => {
+
+type HeaderProps = {
+  isVisible: boolean;
+  setIsVisible: (visible: boolean) => void;
+};
+
+export const Header = ({ isVisible, setIsVisible }: HeaderProps) => {
   return (
     <header className="header">
       <h3 className="header-logo">grpsm</h3>
       <ul className="header-list">
         <li className="list-item">
-          <button className="item-connection">➕</button>
+          <button
+            className="item-connection"
+            onClick={() => setIsVisible(!isVisible)}
+          >
+            ➕
+          </button>
         </li>
         <li className="list-item">
           <button className="item-start">⏩</button>
