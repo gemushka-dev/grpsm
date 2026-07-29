dc5cd2c91e4c04ab41c5caf25d5e244bf46d7729
unknown <iskandar.django.work@gmail.com>
2026-07-29 13:40:02 +0500

    feat(db-info): add database schema tree in sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import { Connection } from "../type/connection.type";
 import "../styles/sidebar.css";
+import { invoke } from "@tauri-apps/api/core";
 
 type SidebarProps = {
   onNodeSelect: (node: any) => void;
   pgConnections: Connection[];
   setConnections: React.Dispatch<React.SetStateAction<Connection[]>>;
+};
+
+export type ColumnInfo = {
+  table_name: string;
+  column_name: string;
+  data_type: string;
 };
 
 export const Sidebar = ({
@@ -14,6 +21,9 @@ export const Sidebar = ({
   setConnections,
 }: SidebarProps) => {
   const [selectedId, setSelectedId] = useState("");
+  const [tablesMap, setTablesMap] = useState<
+    Record<string, Record<string, ColumnInfo[]>>
+  >({});
   const handleConnectClick = (conn: Connection) => {
     setSelectedId(conn.id);
     if (onNodeSelect) {
@@ -26,6 +36,18 @@ export const Sidebar = ({
       })),
     );
   };
+
+  const handleDoubleClick = async (conn: Connection) => {
+    const result = await invoke<ColumnInfo[]>("get_db_info", {
+      uuid: conn.id,
+    });
+    const r: Record<string, ColumnInfo[]> = {};
+    for (const item of result) {
+      (r[item.table_name] ??= []).push(item);
+    }
+    setTablesMap((prev) => ({ ...prev, [conn.id]: r }));
+    console.log(result);
+  };
   return (
     <aside className="sidebar">
       <div className="sidebar-header">
@@ -36,25 +58,62 @@ export const Sidebar = ({
       <div className="sidebar-list">
         {pgConnections.map((conn) => {
           const isSelected = selectedId === conn.id;
+          const tables = tablesMap[conn.id];
           return (
-            <div
-              className={`connection-item ${isSelected ? "selected" : ""}`}
-              key={conn.id}
-              onClick={() => handleConnectClick(conn)}
-            >
-              <div className="connection-info">
-                <div className="connection-avatar">PG</div>
-                <div className="connection-details">
-                  <div className="connection-name">{conn.name}</div>
-                  <div className="connection-meta">
-                    {conn.host}:{conn.port} ·{" "}
-                    <span className="db-name"> {conn.dbName}</span>
+            <div className="connection-wrapper">
+              <div
+                className={`connection-item ${isSelected ? "selected" : ""}`}
+                key={conn.id}
+                onClick={() => handleConnectClick(conn)}
+                onDoubleClick={() => handleDoubleClick(conn)}
+              >
+                <div className="connection-info">
+                  <div className="connection-avatar">PG</div>
+                  <div className="connection-details">
+                    <div className="connection-name">{conn.name}</div>
+                    <div className="connection-meta">
+                      {conn.host}:{conn.port} ·{" "}
+                      <span className="db-name"> {conn.dbName}</span>
+                    </div>
                   </div>
                 </div>
+                <div
+                  className={`status-dot ${conn.isConnected ? "online" : "offline"}`}
+                ></div>
               </div>
-              <div
-                className={`status-dot ${conn.isConnected ? "online" : "offline"}`}
-              ></div>
+              {tables && (
+                <div className="tables-tree">
+                  <details>
+                    <summary>{conn.dbName}</summary>
+
+                    <details>
+                      <summary>Schemas</summary>
+
+                      <details>
+                        <summary>Tables</summary>
+
+                        <details>
+                          <summary>Columns</summary>
+                          {Object.entries(tables).map(([tableName, cols]) => (
+                            <details key={tableName}>
+                              <summary>{tableName}</summary>
+
+                              <ul>
+                                {cols.map((col) => (
+                                  <li key={col.column_name}>
+                                    {col.column_name}{" "}
+                                    <small>({col.data_type})</small>
+                                  </li>
+                                ))}
+                              </ul>
+                            </details>
+                          ))}
+                        </details>
+                      </details>
+                    </details>
+                  </details>
+                </div>
+              )}
             </div>
           );
         })}
